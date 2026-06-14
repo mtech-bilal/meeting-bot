9c7d6c2746c9c7e2574ba0ebfc2bf71555f5a281
Bilal <m.bilal2145@gmail.com>
2026-06-14 14:20:29 +0500

    feat: BrowserManager with Playwright Chromium

---
+++ src/browser/BrowserManager.js
@@ -0,0 +1,60 @@
+const { chromium } = require('playwright');
+const os = require('os');
+const config = require('../config');
+const logger = require('../logger');
+
+class BrowserManager {
+  constructor() {
+    this.browser = null;
+    this.context = null;
+    this.page = null;
+  }
+
+  async launch() {
+    logger.info('Launching browser...');
+
+    const isLinux = os.platform() === 'linux';
+
+    const args = [
+      '--use-fake-ui-for-media-stream',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+    ];
+
+    // On Linux (Docker), send silence as mic so no real audio is captured from input
+    if (isLinux) {
+      args.push('--use-file-for-fake-audio-capture=/dev/zero');
+    }
+
+    this.browser = await chromium.launch({
+      headless: config.headless,
+      args,
+    });
+
+    this.context = await this.browser.newContext({
+      permissions: ['microphone', 'camera'],
+      viewport: { width: 1280, height: 720 },
+    });
+
+    this.page = await this.context.newPage();
+
+    this.browser.on('disconnected', () => {
+      logger.error('Browser disconnected unexpectedly');
+    });
+
+    return { browser: this.browser, context: this.context, page: this.page };
+  }
+
+  async close() {
+    if (this.browser) {
+      await this.browser.close().catch(() => {});
+      this.browser = null;
+      this.context = null;
+      this.page = null;
+    }
+  }
+}
+
+module.exports = BrowserManager;
